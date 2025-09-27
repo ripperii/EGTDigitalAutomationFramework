200cebf4cb5bb2c139b1d8a0fe2c78bef57e28e0
Mircho Voynov <mircho.voynov@gmail.com>
2025-09-27 15:57:34 +0300

    Added Allure Test Reports to the automation Framework

--- EGTDigitalAutomationFramework/Tests/API/CreateUserTest.cs
+++ EGTDigitalAutomationFramework/Tests/API/CreateUserTest.cs
@@ -1,4 +1,6 @@
-﻿using EGTDigitalAutomationFramework.Configs;
+﻿using Allure.Net.Commons;
+using Allure.Xunit.Attributes;
+using EGTDigitalAutomationFramework.Configs;
 using EGTDigitalAutomationFramework.Core;
 using EGTDigitalAutomationFramework.Models;
 using EGTDigitalAutomationFramework.Tests.API.Base;
@@ -12,17 +14,25 @@ using System.Threading.Tasks;
 
 namespace EGTDigitalAutomationFramework.Tests.API
 {
-
+    
     public class CreateUserTest : APITestBase
     {
-        [Theory]
+        
+        [Theory(DisplayName = "Verify Creation of user")]
         [InlineData("Test User", "Automation Tester", "reqres-free-v1", HttpStatusCode.Created)]
         [InlineData(null, "Automation Tester", "reqres-free-v1", HttpStatusCode.Created)]
         [InlineData(null, null, "reqres-free-v1", HttpStatusCode.Created)]
         [InlineData("Ivan", "Operator na metla", "", HttpStatusCode.Unauthorized)]
         [InlineData("Gosho", "QA", "ewqdsa", HttpStatusCode.Forbidden)]
+        [AllureDescription("This test verifies that a valid/invalid request for user creation has a successfull expected result.")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureTag("Smoke", "Login")]
+        [AllureOwner("QA-Team")]
+
         public async Task CreateUser(string? name, string? job, string apiKey, HttpStatusCode expected)
         {
+            Console.WriteLine("Create User Test");
+
             RestResponse res = await ApiClient.Instance.CreateUserAsync(new UserData()
             {
                 Name = name,
--- EGTDigitalAutomationFramework/Tests/UI/Base/UIBaseTest.cs
+++ EGTDigitalAutomationFramework/Tests/UI/Base/UIBaseTest.cs
@@ -1,16 +1,14 @@
-﻿using EGTDigitalAutomationFramework.Configs;
-using EGTDigitalAutomationFramework.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EGTDigitalAutomationFramework.Tests.API.Base
+namespace EGTDigitalAutomationFramework.Tests.UI.Base
 {
-    public abstract class APITestBase : IDisposable
+    public abstract class UIBaseTest : IDisposable
     {
-        protected APITestBase() 
+        protected UIBaseTest()
         {
             Console.WriteLine("Setup");
         }
